bcf3620939879379265ff30ca3f5d3a6f7113feb
{Muramara} <sebastianmuramara@gmail.com>
2025-07-18 05:19:18 +0300

    use .env

--- src/api/products.ts
+++ src/api/products.ts
@@ -2,10 +2,10 @@
 import { createClient } from '@supabase/supabase-js';
 import { Product } from '../types';
 
-const supabase = createClient(
-  'https://ctkcjuahsszvjudllcid.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN0a2NqdWFoc3N6dmp1ZGxsY2lkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE1MjU5MTMsImV4cCI6MjA2NzEwMTkxM30.MyqTYoW-45kQu-Gwh6DvFo_Av5gfeWCoU6w3Lfjkaog'
-);
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 export const fetchProducts = async (): Promise<Product[] | null> => {
   const { data, error } = await supabase.from('products').select('*');
